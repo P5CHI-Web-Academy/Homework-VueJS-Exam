d2469c2f8aaa858ec914612d505d3ae8863d6248
Ola del Mar <mashasbc@gmail.com>
2019-06-15 10:22:14 +0300

    Make eslint stricter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,16 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   rules: {
+    'vue/max-attributes-per-line': ['error', { singleline: 7 }],
+    'vue/singleline-html-element-content-newline': ['error', {
+      'ignoreWhenNoAttributes': true,
+      'ignoreWhenEmpty': true,
+      'ignores': ['a', 'li', 'router-link']
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
