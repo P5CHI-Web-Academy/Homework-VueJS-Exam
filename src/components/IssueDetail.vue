362e23409eb321c5fea528d7e8be73c893e0afeb
vstanciu <stanciu_vasile@mail.ru>
2019-06-15 13:35:40 +0300

    exam commit

---
+++ src/components/IssueDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h2>{{issue.title}} #{{issue.number}}</h2>
+      <p>
+        <h5>
+          <span class="badge badge-secondary">{{issue.state}}</span>
+        </h5>
+      </p>
+    </div>
+    <div class="card-body">
+      <p class="card-text">{{issue.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'issueDetail',
+  computed: {
+    ...mapGetters({
+      issue: 'GET_ISSUE_BY_ID'
+    })
+  },
+  mounted () {
+    this.set(this.$route.params.id)
+  },
+  methods: {
+    ...mapActions({
+      set: 'SET_SELECTED_ISSUE'
+    })
+  }
+}
+</script>
