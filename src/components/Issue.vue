c6f106e038dcaed54ef42656e70a27d4a5cb4c9c
Cristian <mlcristianv@gmail.com>
2019-06-15 13:46:16 +0300

    fix some style

--- src/components/Issue.vue
+++ src/components/Issue.vue
@@ -10,10 +10,9 @@
           alt="User"
         >
       </v-avatar>
-      <!-- eslint-disable-next-line vue/no-v-html -->
+
       <p class="mr-3">{{ issue.user.login }}</p>
       <v-spacer />
-      <!-- eslint-disable-next-line vue/no-v-html -->
       <p>{{ issue.body }}</p>
     </v-layout>
     <hr>
--- src/views/Issue.vue
+++ src/views/Issue.vue
@@ -1,6 +1,21 @@
 <template>
 <div>
-   {{ issue.body }}
+  <router-link :to="{ name: 'home' }">Home</router-link>
+  <template>
+    <v-layout>
+      <v-flex sm12 offset-sm3>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ issue.title }}</h3>
+            </div>
+          </v-card-title>
+          <br>
+          {{ issue.body }}
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </template>    
 </div>
 </template>
 
@@ -16,8 +31,7 @@ export default {
   },
   computed: {
     ...mapGetters({
-      getIssueById: 'issues/getIssueById',
-      getClosedIssues: 'issues/getClosedIssues'
+      getIssueById: 'issues/getIssueById'
     }),
     issue () {
       console.log(this.id)
